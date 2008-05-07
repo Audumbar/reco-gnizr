bf456f803f0cef89d855a891b61616d2e3bcd225
kishor_datar@yahoo.com <kishor_datar@yahoo.com@650078fd-2846-0410-94c5-efdd8ba19d2e>
2008-05-07 00:50:05 +0000

    made UI configurable

--- reco-gnizr-client/js/load-reco-gnizr.js
+++ reco-gnizr-client/js/load-reco-gnizr.js
@@ -29,4 +29,24 @@ recognizrFrame.style.border = "none";
 recognizrFrame.style.height = "150px";
 recognizrFrame.style.width = "250px"; 
 recognizrFrame.setAttribute("scrolling", "no");
+
+  var h = options;
+  var properties = {};
+  if(h != "")
+  {
+    h = h.substring(1);
+    var t = [];
+    t = h.split("&");
+    for(var i=0; i<t.length; i++)
+    {
+      var t2 = t[i].split("=");
+      properties[t2[0]] = t2[1];
+    }
+  }
+  if(properties["width"])
+    recognizrFrame.style.width = properties["width"];
+  if(properties["height"])
+    recognizrFrame.style.height = properties["height"];
+
+
 thisElement.parentNode.appendChild(recognizrFrame);
